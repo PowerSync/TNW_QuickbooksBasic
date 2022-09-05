a9cbac5ad30170b42affbacb9b4eaa6cd83be521
Pavel Bystritsky <p.bystritskyi@gmail.com>
2022-09-05 13:29:39 +0200

    TNW-1622: [Marketplace] Availability of our modules.

--- .circleci/submit_code.sh
+++ .circleci/submit_code.sh
@@ -1,19 +1,42 @@
+#set -x
+
 VERSION=$(grep -o '^ *"version": *"[0-9\.]*"' ../composer.json | awk '{print $2}' | sed -e 's/"\(.*\)"/\1/g')
 WD=$(pwd)
 
-# Create package update payload
-ARCH_NAME=code.zip
+# Create shared package update payload
+SHARED_ARCH_NAME=code_shared.zip
 cd ..
-zip -r $ARCH_NAME ./*
+zip -r $SHARED_ARCH_NAME ./*
 
-# Upload package update to Marketplace
+# Upload shared package update to Marketplace
 cd "${WD}"
 git clone git@github.com:PowerSync/TNW_EQP.git eqp --branch main
 [ -f .env ] && cp .env eqp
-mv ../$ARCH_NAME eqp
-cp -r package/data eqp
+mv ../$SHARED_ARCH_NAME eqp
+cp -r sharedpackage/data eqp
 cd eqp
-bin/main $ARCH_NAME $VERSION 0
+bin/main $SHARED_ARCH_NAME $VERSION 0
 RESULT=$?
-rm $ARCH_NAME
+rm data
+rm $SHARED_ARCH_NAME
+if [ $RESULT -ne 0 ]; then
+    exit $RESULT
+fi
+
+# Create meta package update payload
+cd "${WD}"
+META_ARCH_NAME=code_meta.zip
+SAMPLE=metapackage/composer.json.sample
+TARGET=metapackage/composer.json
+cp $SAMPLE $TARGET
+sed -i "s/\$VERSION/$VERSION/" $TARGET
+zip -j $META_ARCH_NAME $TARGET
+
+# Upload update to Marketplace
+mv $META_ARCH_NAME eqp
+cp -r metapackage/data eqp
+cd eqp
+bin/main $META_ARCH_NAME $VERSION 1
+RESULT=$?
+rm $META_ARCH_NAME
 exit $RESULT
