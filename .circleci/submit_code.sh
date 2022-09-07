5b34fc123818b2e12ab8a0ef3aa87b01d94c28d6
Pavel Bystritsky <p.bystritskyi@gmail.com>
2022-09-07 11:08:09 +0200

    TNW-1622: [Marketplace] Availability of our modules.

--- .circleci/submit_code.sh
+++ .circleci/submit_code.sh
@@ -23,7 +23,7 @@ if [ $RESULT -ne 0 ]; then
     exit $RESULT
 fi
 
-# Create meta package update payload
+# Create basic meta package update payload
 cd "${WD}"
 META_ARCH_NAME=code_meta.zip
 SAMPLE=metapackage/composer.json.sample
@@ -32,11 +32,68 @@ cp $SAMPLE $TARGET
 sed -i "s/\$VERSION/$VERSION/" $TARGET
 zip -j $META_ARCH_NAME $TARGET
 
-# Upload update to Marketplace
+# Upload basic meta package to Marketplace
 mv $META_ARCH_NAME eqp
 cp -r metapackage/data eqp
 cd eqp
 bin/main $META_ARCH_NAME $VERSION 1
 RESULT=$?
 rm $META_ARCH_NAME
+exit $RESULT
+
+# Create advanced meta package update payload
+if [[ -z "${EQP_ADVANCED_META_SKU}" ]]; then
+  echo 'Environment variable EQP_ADVANCED_META_SKU not defined.'
+  exit 1
+fi
+if [[ -z "${EQP_SHARED_MODULES}" ]]; then
+  echo 'Environment variable EQP_SHARED_MODULES not defined.'
+  exit 2
+fi
+if [[ -z "${EQP_BASIC_SKU}" ]]; then
+  echo 'Environment variable EQP_BASIC_SKU not defined.'
+  exit 3
+fi
+if [[ -z "${EQP_ENTERPRISE_SKU}" ]]; then
+  echo 'Environment variable EQP_ENTERPRISE_SKU not defined.'
+  exit 4
+fi
+if [[ -z "${EQP_BUSINESS_SKU}" ]]; then
+  echo 'Environment variable EQP_BUSINESS_SKU not defined.'
+  exit 5
+fi
+cd "${WD}"
+ADVANCED_META_ARCH_NAME=code_meta_advanced.zip
+SAMPLE=advancedmetapackage/composer.json.sample
+TARGET=advancedmetapackage/composer.json
+cp $SAMPLE $TARGET
+cd eqp
+bin/get_next_version "${EQP_ADVANCED_META_SKU}"
+bin/get_package_versions "${EQP_SHARED_MODULES}"
+ENCODED_SKU=$(php -r "echo urlencode('"EQP_ADVANCED_META_SKU"');")
+NEXT_VERSION=$(cat "tmp/next/${ENCODED_SKU}")
+BASIC_ENCODED_SKU=$(php -r "echo urlencode('"EQP_BASIC_SKU"');")
+BASIC_VERSION=$(cat "tmp/shared/${BASIC_ENCODED_SKU}")
+ENTERPRISE_ENCODED_SKU=$(php -r "echo urlencode('"EQP_ENTERPRISE_SKU"');")
+ENTERPRISE_VERSION=$(cat "tmp/shared/${ENTERPRISE_ENCODED_SKU}"))
+BUSINESS_ENCODED_SKU=$(php -r "echo urlencode('"EQP_BUSINESS_SKU"');")
+BUSINESS_VERSION=$(cat "tmp/shared/${BUSINESS_ENCODED_SKU}")
+cd "${WD}"
+sed -i "s/\$EQP_ADVANCED_META_SKU/$EQP_ADVANCED_META_SKU/" "$TARGET"
+sed -i "s/\$VERSION/$NEXT_VERSION/" "$TARGET"
+sed -i "s/\$EQP_BASIC_SKU/$EQP_BASIC_SKU/" "$TARGET"
+sed -i "s/\$BASIC_VERSION/$BASIC_VERSION/" "$TARGET"
+sed -i "s/\$EQP_ENTERPRISE_SKU/$EQP_ENTERPRISE_SKU/" "$TARGET"
+sed -i "s/\$ENTERPRISE_VERSION/$ENTERPRISE_VERSION/" "$TARGET"
+sed -i "s/\$EQP_BUSINESS_SKU/$EQP_BUSINESS_SKU/" "$TARGET"
+sed -i "s/\$BUSINESS_VERSION/$BUSINESS_VERSION/" "$TARGET"
+zip -j "$ADVANCED_META_ARCH_NAME" "$TARGET"
+
+# Upload advanced meta package to Marketplace
+mv $ADVANCED_META_ARCH_NAME eqp
+cp -r advancedmetapackage/data eqp
+cd eqp
+bin/main $ADVANCED_META_ARCH_NAME "$VERSION" 1 "$EQP_ADVANCED_META_SKU"
+RESULT=$?
+rm $ADVANCED_META_ARCH_NAME
 exit $RESULT
