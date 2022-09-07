9d37f1595f1cb4c453f422efe2021fe599946e4f
Pavel Bystritsky <p.bystritskyi@gmail.com>
2022-09-07 11:42:27 +0200

    TNW-1622: [Marketplace] Availability of our modules.

--- .circleci/submit_code.sh
+++ .circleci/submit_code.sh
@@ -36,7 +36,10 @@ zip -j $META_ARCH_NAME $TARGET
 mv $META_ARCH_NAME eqp
 cp -r metapackage/data eqp
 cd eqp
+TMP=$EQP_SHARED_MODULES
+EQP_SHARED_MODULES=$EQP_BASIC_SKU # Rewrite EQP_SHARED_MODULES variable as original contains private modules
 bin/main $META_ARCH_NAME $VERSION 1
+EQP_SHARED_MODULES=$TMP # Restore EQP_SHARED_MODULES variable
 RESULT=$?
 rm $META_ARCH_NAME
 exit $RESULT
